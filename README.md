b778c58b9f944a862eec971a0724591c9183d555
David <davidernst0902@web.de>
2025-04-02 20:58:45 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Beispiel PHP Projeckt für Mitschüler
+
+Ein simples Beispiel Projekt für meine Klasse
+
+In einem Form kann man sein Alter und seinen Namen eingeben und PHP gibt einen Wert wieder
---
+++ phpFormAnswer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Answer</title>
+</head>
+<body>
+    <?php 
+        $user_name = $_POST[user_name];
+        $user_age = $_POST[user_age];
+        echo("Der User " . $user_name . " ist " . $user_age . " Jahre alt!");
+    ?>
+</body>
+</html>
